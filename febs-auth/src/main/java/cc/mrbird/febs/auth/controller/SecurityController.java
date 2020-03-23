7755e1a6dd8529dd46bc3b0bf2a6d4e4458cb9ec
wangyue2 <a*1a*2a*3>
2020-03-23 22:04:40 +0800

    第一次提交

---
+++ febs-auth/src/main/java/cc/mrbird/febs/auth/controller/SecurityController.java
@@ -0,0 +1,41 @@
+package cc.mrbird.febs.auth.controller;
+
+import cc.mrbird.febs.common.entity.FebsResponse;
+import cc.mrbird.febs.common.exception.FebsAuthException;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
+
+@RestController
+public class SecurityController {
+
+    @Autowired
+    private ConsumerTokenServices consumerTokenServices;
+
+    @GetMapping("oauth/test")
+    public String testOauth() {
+        return "oauth";
+    }
+
+    @GetMapping("user")
+    public Principal currentUser(Principal principal) {
+        return principal;
+    }
+
+    @DeleteMapping("signout")
+    public FebsResponse signout(HttpServletRequest request) throws FebsAuthException {
+        String authorization = request.getHeader("Authorization");
+        String token = StringUtils.replace(authorization, "bearer ", "");
+        FebsResponse febsResponse = new FebsResponse();
+        if (!consumerTokenServices.revokeToken(token)) {
+            throw new FebsAuthException("退出登录失败");
+        }
+        return febsResponse.message("退出登录成功");
+    }
+}
---
+++ febs-common/src/main/java/cc/mrbird/febs/common/exception/FebsAuthException.java
@@ -0,0 +1,10 @@
+package cc.mrbird.febs.common.exception;
+
+public class FebsAuthException  extends Exception{
+
+    private static final long serialVersionUID = -6916154462432027437L;
+
+    public FebsAuthException(String message){
+        super(message);
+    }
+}
